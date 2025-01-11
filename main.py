3a4417b1942affb01db9da7a8df1fef07de2e0e6
dominikstipic <dominikstipic@gmail.com>
2025-01-11 17:13:44 +0100

    initial

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, send_file
+from waitress import serve
+
+app = Flask(__name__, template_folder=".")
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/pgp")
+def encrypt():
+    return send_file('pub.txt')
+
+
+if __name__ == "__main__":
+    print("Starting server!")
+    serve(app, host="0.0.0.0", port=85)
+    #app.run(host="127.0.0.1", port=8080, debug=True)#
